1fd6e60f80ad0e2ba734c653f050f208defa39ff
Pavel Varnavsky <varnavsky.pavel@gmail.com>
2016-10-04 16:01:22 +0300

    Add hours_ahead func

--- mysite/views.py
+++ mysite/views.py
@@ -1,10 +1,21 @@
-from django.http import HttpResponse
-from datetime import datetime
+from django.http import Http404, HttpResponse
+import datetime
 
 def hello(request):
     return HttpResponse('Hello world')
 
 def current_datetime(request):
-    now = datetime.now()
+    now = datetime.datetime.now()
     response = "<html><body>Now: %s </body></html>" % now
+    return HttpResponse(response)
+
+def hours_ahead(request, offset):
+    try:
+        offset = int(offset)
+    except ValueError:
+        raise Http404()
+
+    dt = datetime.datetime.now() + datetime.timedelta(hours=offset)
+    response = "<html><body>Через %s часов будет%s.</body></html>" % (offset, dt)
+
     return HttpResponse(response)
