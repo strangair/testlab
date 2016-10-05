b173a68bffe7337f2dfd4599902048f21994ed49
Pavel Varnavsky <varnavsky.pavel@gmail.com>
2016-10-05 10:10:27 +0300

    Add templates

--- mysite/views.py
+++ mysite/views.py
@@ -1,6 +1,9 @@
 # coding: utf-8
 
 from django.http import Http404, HttpResponse
+from django.template.loader import get_template
+from django.template import Context
+
 import datetime
 
 def hello(request):
@@ -8,8 +11,9 @@ def hello(request):
 
 def current_datetime(request):
     now = datetime.datetime.now()
-    response = "<html><body>Now: %s </body></html>" % now
-    return HttpResponse(response)
+    t = get_template('current_datetime.html')
+    html = t.render(Context({'current_date': now}))
+    return HttpResponse(html)
 
 def hours_ahead(request, offset):
     try:
@@ -18,6 +22,6 @@ def hours_ahead(request, offset):
         raise Http404()
 
     dt = datetime.datetime.now() + datetime.timedelta(hours=offset)
-    response = "<html><body>Через %s часов будет%s.</body></html>" % (offset, dt)
+    response = "<html><body>Через %s часов будет %s.</body></html>" % (offset, dt)
 
     return HttpResponse(response)
