61880b291aebbc2e1bfbccca68d3a63722242169
Pavel Varnavsky <varnavsky.pavel@gmail.com>
2016-10-05 13:08:24 +0300

    Sync

--- mysite/settings.py
+++ mysite/settings.py
@@ -75,12 +75,12 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    },
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    },
 
-    'mysite': {
+    'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'mysite',
         'USER': 'django',
