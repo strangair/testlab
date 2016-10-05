65575c42da92205a197811c69b3612ab11cb2fe7
Pavel Varnavsky <varnavsky.pavel@gmail.com>
2016-10-05 16:06:26 +0300

    Sync

--- books/views.py
+++ books/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, render_to_response
 
 # Create your views here.
+
+def search_form(request):
+    return render_to_response('search_form.html')
+
--- mysite/urls.py
+++ mysite/urls.py
@@ -23,4 +23,5 @@ urlpatterns = [
     url(r'^hello/$', hello),
     url(r'^time/$', current_datetime),
     url(r'^time/plus/(\d{1,2})/$', hours_ahead),
+    url(r'^search-form/$', search_form)
 ]
