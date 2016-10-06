bc1c452ffc20db0cffab9ef59d6bcd70900916fb
Pavel Varnavsky <varnavsky.pavel@gmail.com>
2016-10-06 12:24:17 +0300

    Sync

--- books/views.py
+++ books/views.py
@@ -11,9 +11,12 @@ def search_form(request):
     return render_to_response('search_form.html')
 
 def search(request):
-    if 'q' in request.GET and request.GET['q']:
+    error = False
+    if 'q' in request.GET:
         q = request.GET['q']
-        books = Book.objects.filter(title__icontains=q)
-        return render_to_response('search_results.html', {'books': books, 'query': q})
-    else:
-        return render_to_response('search_form.html', {'error': True})
+        if not q:
+            error = True
+        else:
+            books = Book.objects.filter(title__icontains=q)
+            return render_to_response('search_results.html', {'books': books, 'query': q})
+    return render_to_response('search_form.html', {'error': error})
